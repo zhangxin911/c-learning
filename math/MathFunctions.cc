474cd2591f3dfb7fe208d43483edfd15fde0f516
zhangxin <xin1.zhang@haiziwang.com>
2021-04-25 23:14:04 +0800

    day88

--- math/MathFunctions.cc
+++ math/MathFunctions.cc
@@ -1,18 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "MathFunctions.h"
+#include <iostream>
 
 double power(double base, int exponent)
 {
   int result = base;
   int i;
-  
-  if (exponent == 0) {
+
+  if (exponent == 0)
+  {
     return 1;
   }
-  
-  for(i = 1; i < exponent; ++i){
+
+  for (i = 1; i < exponent; ++i)
+  {
     result = result * base;
   }
   return result;
+}
+
+double min(double a, double b)
+{
+  return a < b ? a : b;
+}
+
+void chline(char ch, int i, int j)
+{
+  using namespace std;
+  if (i < j)
+  {
+    for (int x = 0; x < j; x++)
+    {
+      if (x == i)
+      {
+        std::cout << ch << std::endl;
+      }
+    }
+  }
+  else
+  {
+    for (int x = 0; x < i; x++)
+    {
+      std::cout << ch << std::endl;
+    }
+  }
 }
--- math/MathFunctions.h
+++ math/MathFunctions.h
@@ -1,3 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
 double power(double base, int exponent);
+double min(double a, double b);
+void chline(ch,i,j);

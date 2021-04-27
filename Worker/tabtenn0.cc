cd52f826cb5c5b78f702cb085ff837c066e01d61
zhangxin <xin1.zhang@haiziwang.com>
2021-04-27 22:20:56 +0800

    day89

---
+++ Worker/tabtenn0.cc
@@ -0,0 +1,27 @@
+#include "tabtenn0.h"
+#include <iostream>
+TableTennisPlayer::TableTennisPlayer(const string &fn, const string &ln, bool ht) : firstname(fn), lastname(ln), hasTable(ht)
+{
+}
+
+void TableTennisPlayer::Name() const
+{
+    std::cout << firstname << ", " << lastname << std::endl;
+}
+
+TableTennisPlayer::TableTennisPlayer(const string &fn, const string &ln, bool ht)
+{
+    firstname = fn;
+    lastname = ln;
+    hasTable = ht;
+}
+
+RatedPlayer::RatedPlayer(unsigned int r, const string &fn, const string &ln, bool ht) : TableTennisPlayer(fn, ln, ht)
+{
+    rating = r;
+}
+
+RatedPlayer::RatedPlayer(unsigned int r, const TableTennisPlayer &tp) : TableTennisPlayer(tp),rating(r)
+{
+    rating = r;
+}

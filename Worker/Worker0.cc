c81264293daf5c6933376776f45119da722edc22
zhangxin <453790557@qq.com>
2021-05-07 21:50:24 +0800

    day95

--- Worker/Worker0.cc
+++ Worker/Worker0.cc
@@ -1,8 +1,75 @@
 #include "Worker0.h"
-Worker0::Worker0(/* args */)
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+Worker::~Worker()
 {
 }
 
-Worker0::~Worker0()
+void Worker::Set()
 {
+    cout << "Enter worker's name: ";
+    getline(cin, fullname);
+    cout << "Enter worker's ID: ";
+    cin >> id;
+    while (cin.get() != '\n')
+        continue;
+}
+
+void Worker::Show() const
+{
+    cout << "Name: " << fullname << "\n";
+    cout << "Employee ID: " << id << "\n";
+}
+
+void Waiter::Set()
+{
+    Worker::Set();
+    cout << "Enter waiter's panache rating: ";
+    cin >> panache;
+    while (cin.get() != '\n')
+        continue;
+}
+
+void Waiter::Show() const
+{
+    cout << "Category: waiter\n";
+    Worker::Show();
+    cout << "Panache rating: " << panache << "\n";
+}
+
+char *Singer::pv[] = {
+    "other",
+    "alto",
+    "contralto",
+    "soprano",
+    "bass",
+    "baritone",
+    "tenor"};
+
+void Singer::Set()
+{
+    Worker::Set();
+    cout << "Enter number for singer's vocal range:\n";
+    int i;
+    for (i = 0; i < Vtypes; i++)
+    {
+        cout << i << ": " << pv[i] << "  ";
+        if (i % 4 == 3)
+            cout << endl;
+    }
+    if (i % 4 != 0)
+        cout << endl;
+    while (cin >> voice && (voice < 0) || voice >= Vtypes)
+        cout << "Please enter a value >=0 and <" << Vtypes << endl;
+    while (cin.get() != '\n')
+        continue;
+}
+
+void Singer::Show() const
+{
+    cout << "Category: singer\n";
+    Worker::Show();
+    cout << "Vocal range: " << pv[voice] << endl;
 }
--- Worker/Worker0.h
+++ Worker/Worker0.h
@@ -2,18 +2,61 @@
 #define Worker0_h
 #include <string>
 
-class Worker0
+class Worker
 {
 private:
     std::string fullname;
     long id;
 
 public:
-    Worker0(/* args */) : fullname("no one"), id(0L){};
-    Worker0(const std::string &s, long n) : fullname(s), id(n){};
-    virtual ~Worker0() = 0;
-    virtual void set();
+    Worker(/* args */) : fullname("no one"), id(0L){};
+    Worker(const std::string &s, long n) : fullname(s), id(n){};
+    virtual ~Worker() = 0;
+    virtual void Set();
     virtual void Show() const;
+};
+
+class Waiter : public Worker
+{
+private:
+    int panache;
+
+public:
+    Waiter() : Worker(), panache(0) {}
+    Waiter(const std::string &s, long n, int p = 0) : Worker(s, n), panache(p) {}
+    Waiter(const Worker &wk, int p = 0) : Worker(wk), panache(p) {}
+    void Set();
+    void Show() const;
+};
+
+class Singer : public Worker
+{
+protected:
+    enum
+    {
+        other,
+        alto,
+        contrallo,
+        soprano,
+        bass,
+        baritone,
+        tenor
+    };
+    enum
+    {
+        Vtypes = 7
+    };
+
+private:
+    static char *pv[Vtypes];
+    int voice;
+
+public:
+    Singer() : Worker(), voice(other) {}
+    Singer(const std::string &s, long n, int v = other) : Worker(s, n), voice(v) {}
+    Singer(const Worker &wk, int v = other) : Worker(wk), voice(v) {}
+    void Set();
+    void Show() const;
 };
 
 #endif

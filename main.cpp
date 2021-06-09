dd6e9791aac65078c0bcaa062ac6b191bd8ae624
zhangxin <453790557@qq.com>
2021-06-09 10:13:22 +0800

    day110

--- main.cpp
+++ main.cpp
@@ -1,83 +1,104 @@
 #include <iostream>
 #include "test.h"
+// typedef struct linkListNode
+// {
+//     int value;
+//     struct linkListNode *next;
+// } lld;
 
-typedef struct student
+typedef struct linkListNode
 {
-    int age;
-    char name[10];
-    int num;
-} stu_t;
+    int value;
+    linkListNode *next;
+} lld;
+
+void test(int (*p)(int a), int a)
+{
+    (*p)(a);
+    std::cout << a << std::endl;
+}
+
+typedef int(FUNCTION)(int a);
+typedef int (*FUNCTION_P)(int b);
+
+int inc(int a)
+{
+    std::cout << "my_func" << std::endl;
+    return a;
+}
+
+void showStr(char *str)
+{
+    while (*str != '\0')
+    {
+        std::cout << *str << std::endl;
+        str++;
+    }
+}
+
+int fb_arr(int pos)
+{
+    if (pos == 1 || pos == 2)
+    {
+        return 1;
+    }
+    else
+    {
+        return fb_arr(pos - 1) + fb_arr(pos - 2);
+    }
+}
 
 int main(int, char **)
 {
+    FUNCTION *function1 = &inc;
+    FUNCTION_P function2 = &inc;
+    int (*FUNCTION_POINTER)(int c) = &inc;
+    function1(2);
+    function2(3);
+    FUNCTION_POINTER(5);
+    test(&inc, 1);
+    test(function2, 4);
+    test(FUNCTION_POINTER, 5);
+    char *function_str = "function_str";
+    showStr(function_str);
 
-    stu_t stu[4] = {
-        {10, 'stu_a', 1},
-        {21, 'stu_b', 2},
-        {32, 'stu_c', 3},
-        {43, 'stu_d', 4}};
+    std::cout << fb_arr(5) << std::endl;
+    // lld d4 = {44, NULL};
+    // lld d3 = {3, &d4};
+    // lld d2 = {2, &d3};
+    // lld d1 = {1, &d2};
+    // linkListNode link_arr[4] = {
+    //     d1,
+    //     d2,
+    //     d3,
+    //     d4};
+    // for (int i = 0; i < sizeof(link_arr) / sizeof(linkListNode); i++)
+    // {
+    //     printf("%d\n", link_arr[i].value);
+    // }
 
-    FILE *fp = fopen("fwrite&fread.log", "wb");
-    if (fp == NULL)
+    lld *d4 = (lld *)malloc(sizeof(lld));
+    d4->value = 42;
+    d4->next = NULL;
+
+    lld *d3 = (lld *)malloc(sizeof(lld));
+    d3->value = 32;
+    d3->next = d4;
+
+    lld *d2 = (lld *)malloc(sizeof(lld));
+    d2->value = 22;
+    d2->next = d3;
+
+    lld *d1 = (lld *)malloc(sizeof(lld));
+    d1->value = 12;
+    d1->next = d2;
+
+    lld *p = d1;
+    while (p != NULL)
     {
-        perror("fopen error");
-        return -1;
+        printf("%d\n", p->value);
+        p = p->next;
     }
-    fwrite(&stu[1], sizeof(stu_t), 1, fp);
-    fclose(fp);
 
-    FILE *fp_read = fopen("fwrite&fread.log", "rb");
-    stu_t s1;
-
-    int ret = fread(&s1, sizeof(stu_t), 1, fp_read);
-    printf("ret: %d\n", ret);
-
-    printf("age:%d\n", s1.age);
-    printf("name:%s\n", s1.name);
-    printf("num:%d\n", s1.num);
-    fclose(fp_read);
-
-    Test my_test1;
-    char *test = "my_test2";
-    Test my_test2(test);
-    Test my_test3 = "my_test3";
-
-    FILE *fp_cal = fopen("calc.txt", "wr");
-    fputs("10+4=\n", fp_cal);
-    char cal_buf[1024] = {0};
-    fputs("1*2=\n", fp_cal);
-    fclose(fp_cal);
-    FILE *fp_cal_r = fopen("calc.txt", "r");
-    fgets(cal_buf, 4096, fp_cal_r);
-    int cal_a, cal_b;
-    char cal_add;
-    char cal_equal;
-    printf("%s\n", cal_buf);
-    sscanf(cal_buf, "%d%c%d=\n", &cal_a, &cal_add, &cal_b);
-    printf("%d%c%d\n", cal_a, cal_add, cal_b);
-    // fgets("%d%c%d%c\n",fp_cal);
-    fclose(fp_cal_r);
-
-    FILE *fp_read1 = fopen("./test.h", "r");
-    FILE *fp_copy = fopen("copy_test.txt", "w");
-    char copy_buf[128] = {0};
-    int ret1 = 0;
-    if (fp_read1 == NULL)
-    {
-        perror("fp_read1 fail");
-    }
-    while (1)
-    {
-        memset(copy_buf, 0, sizeof(copy_buf));
-        ret1 = fread(copy_buf, 1, sizeof(copy_buf), fp_read1);
-        printf("%d\n", ret1);
-        if (ret1 == 0)
-        {
-            break;
-        }
-        printf("%d\n", ret1);
-        // fwrite(copy_buf, 1, ret, fp_copy);
-    }
-    fclose(fp_read1);
-    fclose(fp_copy);
+    return 1;
 }

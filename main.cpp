29bc6166d5f00d8f89a3c5f2d40e09b45d0c88e5
zhangxin <453790557@qq.com>
2021-06-20 22:40:10 +0800

    day110

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 #include "test.h"
+#include <string>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
 // typedef struct linkListNode
 // {
 //     int value;
@@ -44,10 +49,26 @@ void wrap_function(void (*p)(int a), double b)
     std::cout << b << std::endl;
 }
 
+struct linkNode
+{
+    int value;
+    linkNode *next;
+};
+
+struct linkList
+{
+    linkNode arr[10];
+};
+
 typedef void(FUNCTION)(int a);
 typedef void (*FUNCTION_P)(int a);
-int main(int, char **)
+int main(int argc, char *argv[])
 {
+    // for (size_t index = 0; index < argc; index++)
+    // {
+    //     printf("%s\n", argv[index]);
+    // }
+
     FUNCTION *function = &test_function;
     function(2);
     FUNCTION_P function_p = &test_function;
@@ -99,5 +120,69 @@ int main(int, char **)
         p = p->next;
     }
 
+    // printf("before fork: [%d]\n", getpid());
+    // pid_t pid = fork();
+    // if (pid < 0)
+    // {
+    //     perror("fork fail");
+    //     return -1;
+    // }
+    // else if (pid > 0)
+    // {
+    //     printf("father: pid:[%d]\n", getpid());
+    //     // sleep(5);
+    // }
+    // else if (pid == 0)
+    // {
+    //     printf("child: pid:[%d]\n", getpid());
+    // }
+
+    // printf("after fork: [%d]\n", getpid());
+
+    // printf("before fork:[%d]\n", getpid());
+
+    // for (size_t i = 0; i < 3; i++)
+    // {
+    //     pid_t pid = fork();
+    //     if (pid < 0)
+    //     {
+    //         perror("fork fail");
+    //     }
+    //     else if (pid > 0)
+    //     {
+    //         printf("father pid:[%d], fpid:[%d]\n", getpid(), getppid());
+    //     }
+    //     else if (pid == 0)
+    //     {
+    //         printf("child pid:[%d], fpid:[%d]\n", getpid(), getppid());
+    //         break;
+    //     }
+    // }
+
+    printf("before fork: [%d]\n", getpid());
+    pid_t pid = fork();
+    int fork_val = 100;
+    if (pid < 0)
+    {
+        perror("fork fail\n");
+    }
+    else if (pid > 0)
+    {
+        printf("father fork: [%d]\n", getpid());
+        fork_val++;
+        printf("fork_val:%d\n", fork_val);
+        int status;
+        pid_t wpid = wait(&status);
+        printf("wpid: [%d]", wpid);
+        printf("staus: [%d]", status);
+    }
+    else
+    {
+        // execl("/bin/ls", "ls", "-al", NULL);
+        // perror("execel error!");
+        printf("child fork: [%d]\n", getpid());
+        printf("fork_val:%d\n", fork_val);
+        sleep(2);
+    }
     return 1;
 }
